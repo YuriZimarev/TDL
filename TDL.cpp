db71fbede8e8971ab75f6f273159b24e06db186e
YuriZimarev <yurvanchik@mail.ru>
2023-01-25 18:56:33 +0400

    Main window menu creation (Sub)
    Trying to create main window menu with SubMenu (unsuccessful)

--- TDL.cpp
+++ TDL.cpp
@@ -1,8 +1,48 @@
 #include "TDL.h"
 #include "main.h"
+
+void TDL::SubCommonMenu(Bar& bar){
+	bar.Add("Open", [=]{Exit();});
+	PromptOKCancel("dfs");
+}
+void TDL::CommonMenu(Bar& bar){
+	
+	bar.Sub("Menu", [=](Bar& bar) { SubCommonMenu(bar);});
+	bar.Add("Open", [=]{Exit();});
+}
+void TDL::ProjectMenu(Bar& bar){
+	
+}
+void TDL::EditMenu(Bar& bar){
+	
+}
+void TDL::ViewMenu(Bar& bar){
+	
+}
+void TDL::PageMenu(Bar& bar){
+	
+}
+void TDL::HelpMenu(Bar& bar){
+	
+}
 TDL::TDL()
 {
 	CtrlLayout(*this, "TitanDAW");
 	Title("TitanDAW").Zoomable().Sizeable().Maximize();
+	AddFrame(menu);
+	AddFrame(status);
+	status = "Welcome to the TDL";
+	menu.Set([=](Bar& bar) { CommonMenu(bar);});
+	menu.Set([=](Bar& bar) { ProjectMenu(bar); });
+	menu.Set([=](Bar& bar) { EditMenu(bar); });
+	menu.Set([=](Bar& bar) { ViewMenu(bar); });
+	menu.Set([=](Bar& bar) { PageMenu(bar); });
+	menu.Set([=](Bar& bar) { HelpMenu(bar); });
+	
 }
-
+/*
+Через Sub добавление меню не работает
+Попробую другим путем через bar.Add
+но THISBACK и typedef * CLASSNAME
+может что-нибудь поломать потом
+*/
--- main.h
+++ main.h
@@ -2,10 +2,22 @@
 #define _TDL_main_h_
 
 class TDL : public WithTDLLayout<TopWindow> {
+	MenuBar menu;
+	StatusBar status;
 	void Close() override{
 	delete this;
 }
 public:
+	void CommonMenu(Bar& bar);
+	void ProjectMenu(Bar& bar);
+	void EditMenu(Bar& bar);
+	void ViewMenu(Bar& bar);
+	void PageMenu(Bar& bar);
+	void HelpMenu(Bar& bar);
+	void SubCommonMenu(Bar& bar);
 	TDL();
 };
 #endif
+/*
+
+*/
